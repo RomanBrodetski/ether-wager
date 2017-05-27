07f0ce80be8e82644a42f534194d1199677f7160
Alla Lipina <alla.lipina@xing.com>
2017-05-27 17:58:25 +0200

    Add toggle logic for my positions

--- app/js/components/App.jsx
+++ app/js/components/App.jsx
@@ -20,13 +20,20 @@ class App extends React.Component {
 
     this.choosePositions = this.choosePositions.bind(this);
     this.toggle = this.toggle.bind(this);
+    this.switchTabs = this.switchTabs.bind(this);
 
     CfdMarket.OracleRespond({from: web3.eth.accounts}, 'latest').then(this.handleBlockchainTradeEvent.bind(this))
   }
 
-  toggle(event) {
+  switchTabs(event) {
     this.setState({
       ui: Object.assign(this.state.ui, { currentTab: event.target.name })
+    })
+  }
+
+  toggle(event) {
+    this.setState({
+      ui: Object.assign(this.state.ui, { myPositionsOpen: !this.state.ui.myPositionsOpen })
     })
   }
 
@@ -105,15 +112,24 @@ class App extends React.Component {
           </nav>
 
           <div className="container-fluid">
-
+            <div className="row">
+              <a role="button" className="col-md-12" onClick={this.toggle} href="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                My positions overview
+                {
+                  this.state.ui.myPositionsOpen
+                  ? <span className="glyphicon glyphicon-chevron-down" aria-hidden="true"></span>
+                  : <span className="glyphicon glyphicon-chevron-up" aria-hidden="true"></span>
+                }
+              </a>
+            </div>
             <div className="row">
               <div className={this.state.ui.myPositionsOpen ? "collapse.in" : "collapse"}>
                 <div className="well">
                   <ul className="nav nav-tabs" role="tablist">
-                    <li role="presentation" className={this.state.ui.currentTab === "allPositions" ? "active" : ""}><a href="#" name="allPositions" onClick={this.toggle}>All my positions</a></li>
-                    <li role="presentation" className={this.state.ui.currentTab === "isActive" ? "active" : ""}><a href="#" name="isActive" onClick={this.toggle}>Active positions</a></li>
-                    <li role="presentation" className={this.state.ui.currentTab === "needsAttention" ? "active" : ""}><a href="#" name="needsAttention" onClick={this.toggle}>Pending / Claim / Waiting for oracle</a></li>
-                    <li role="presentation" className={this.state.ui.currentTab === "isClosed" ? "active" : ""}><a href="#" name="isClosed" onClick={this.toggle}>Closed</a></li>
+                    <li role="presentation" className={this.state.ui.currentTab === "allPositions" ? "active" : ""}><a href="#" name="allPositions" onClick={this.switchTabs}>All my positions</a></li>
+                    <li role="presentation" className={this.state.ui.currentTab === "isActive" ? "active" : ""}><a href="#" name="isActive" onClick={this.switchTabs}>Active positions</a></li>
+                    <li role="presentation" className={this.state.ui.currentTab === "needsAttention" ? "active" : ""}><a href="#" name="needsAttention" onClick={this.switchTabs}>Pending / Claim / Waiting for oracle</a></li>
+                    <li role="presentation" className={this.state.ui.currentTab === "isClosed" ? "active" : ""}><a href="#" name="isClosed" onClick={this.switchTabs}>Closed</a></li>
                   </ul>
 
                   <div>
@@ -134,38 +150,40 @@ class App extends React.Component {
               </div>
             </div>
 
-            <div className="col-md-2">
-              <SelectSymbol
-                counters={this.state.counters}
-                changeSymbol={this.changeSymbol}
-                activeSymbol={this.state.activeSymbol}
-                symbols={this.props.symbols} />
-            </div>
+            <div className="row">
+              <div className="col-md-2">
+                <SelectSymbol
+                  counters={this.state.counters}
+                  changeSymbol={this.changeSymbol}
+                  activeSymbol={this.state.activeSymbol}
+                  symbols={this.props.symbols} />
+              </div>
 
-            <div className="col-md-7">
-              <h4>Open Orders</h4>
-              <div className="row">
-                <div className="col-md-12">
-                  {
-                    this.state.orders === undefined
-                      ? <h1>Loading...</h1>
-                      : <OrderBook
-                          orders={this.state.orders.filter((order) => order.symbol === this.state.activeSymbol)}
-                          onTrade={this.loadBlockchainData} />
-                  }
-                  <div>
-                    <TradingView symbol={this.state.symbols[this.state.activeSymbol].tv} />
+              <div className="col-md-7">
+                <h4>Open Orders</h4>
+                <div className="row">
+                  <div className="col-md-12">
+                    {
+                      this.state.orders === undefined
+                        ? <h1>Loading...</h1>
+                        : <OrderBook
+                            orders={this.state.orders.filter((order) => order.symbol === this.state.activeSymbol)}
+                            onTrade={this.loadBlockchainData} />
+                    }
+                    <div>
+                      <TradingView symbol={this.state.symbols[this.state.activeSymbol].tv} />
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <div className="col-md-3">
-              <div className="row">
-                <div className="col-md-12">
-                  <CreateOrder
-                    oracle={this.state.oracles[this.state.activeSymbol] || {oracle: "...", price: "..."}}
-                    symbol={this.state.symbols[this.state.activeSymbol]}
-                    onTrade={this.loadBlockchainData} />
+              <div className="col-md-3">
+                <div className="row">
+                  <div className="col-md-12">
+                    <CreateOrder
+                      oracle={this.state.oracles[this.state.activeSymbol] || {oracle: "...", price: "..."}}
+                      symbol={this.state.symbols[this.state.activeSymbol]}
+                      onTrade={this.loadBlockchainData} />
+                  </div>
                 </div>
               </div>
             </div>
