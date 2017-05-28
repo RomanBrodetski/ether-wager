d5c85f2ca854d5b2cc39e256eca6f576d712f928
Alla Lipina <alla.lipina@xing.com>
2017-05-28 19:32:53 +0200

    Ugly usage of tooltips

--- app/js/components/OrderBook.jsx
+++ app/js/components/OrderBook.jsx
@@ -9,23 +9,28 @@ class OrderBook extends React.Component {
 
   render() {
     return (
-      <table className="table table-hover">
-        <thead>
-          <tr>
-            <th>Type</th>
-            <th>Collateral ETH</th>
-            <th>Strike Price</th>
-            <th>Expires</th>
-            <th>*Input*</th>
-            <th>*Action*</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.props.orders.map((order) => (
-            <OrderRow order={order} onTrade={this.props.onTrade} key={order.id} oracle={this.props.oracle}/>
-          ))}
-        </tbody>
-      </table>
+      <div>
+        {this.props.orders.length === 0
+          ? <p className="text-primary">There are no orders yet.</p>
+          : <table className="table table-hover">
+              <thead>
+                <tr>
+                  <th>Type</th>
+                  <th>Collateral ETH</th>
+                  <th>Strike Price</th>
+                  <th>Expires</th>
+                  <th>*Input*</th>
+                  <th>*Action*</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.props.orders.map((order) => (
+                  <OrderRow order={order} onTrade={this.props.onTrade} key={order.id} oracle={this.props.oracle}/>
+                ))}
+              </tbody>
+            </table>
+        }
+      </div>
     );
   }
 }
