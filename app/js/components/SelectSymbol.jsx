cdc5fec9f50f00c1a14faa86d7902752ff84f275
Alla Lipina <alla.lipina@xing.com>
2017-05-30 16:08:10 +0200

    Modify SelectSymbol

--- app/js/components/SelectSymbol.jsx
+++ app/js/components/SelectSymbol.jsx
@@ -1,7 +1,11 @@
 class SelectSymbol extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      opened: _(_(Object.values(this.props.symbols)).groupBy('group')).mapObject(() => {
+        return true;
+      })
+    }
     this.totalOrders = this.totalOrders.bind(this)
     this.short = this.short.bind(this)
     this.long = this.long.bind(this)
@@ -16,6 +20,18 @@ class SelectSymbol extends React.Component {
     if (this.props.counters[sym])
       return this.props.counters[sym].myShortOrders
 
+  }
+
+  toggle(e, group) {
+    e.preventDefault
+    // opened: {
+      // crypto/btc: true
+      // crypto/usd: true
+      // stocks: true
+    // }
+    this.setState({
+      opened: Object.assign(this.state.opened, { [group]: !this.state.opened[group]})
+    })
   }
 
   long(sym) {
@@ -42,20 +58,33 @@ class SelectSymbol extends React.Component {
           Underlying Asset
         </div>
         <div className="panel-body">
-          <ul className="nav nav-pills nav-stacked">
-            {Object.values(this.props.symbols).map((symbolObj) => (
-              <li key={symbolObj.symbol} className={this.props.activeSymbol === symbolObj.symbol ? "active" : ""}>
-                <a href="" onClick={(e) => this.props.changeSymbol(e, symbolObj.symbol)}>
-                  {symbolObj.caption}
-                  <span className='clearfix' style={{position:'relative',bottom:'2px',float:'right'}}>
-                    {this.spanIfPresent(this.totalOrders(symbolObj.symbol), "label label-default", "total orders")}
-                    {this.spanIfPresent(this.long(symbolObj.symbol), "label label-success", "my long orders")}
-                    {this.spanIfPresent(this.short(symbolObj.symbol), "label label-warning", "my short orders")}
-                  </span>
-                </a>
-              </li>
+          <div className="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+            { _(_(Object.values(this.props.symbols)).groupBy('group')).map((group) => (
+              <div>
+                <div className="panel-heading" style={{borderBottom:'1px solid #dddddd',cursor:'pointer'}} onClick={(e) => this.toggle(e, group[0].group)}>
+                  <h4 className="panel-title">
+                    {group[0].group}
+                  </h4>
+                </div>
+                <div className={this.state.opened[group[0].group] ? "panel-collapse collapse in" : "panel-collapse collapse"}>
+                  <ul className="nav nav-pills nav-stacked">
+                    {Object.values(group).map((symbolObj) => (
+                      <li key={symbolObj.symbol} className={this.props.activeSymbol === symbolObj.symbol ? "active" : ""}>
+                        <a href="" onClick={(e) => this.props.changeSymbol(e, symbolObj.symbol)}>
+                          {symbolObj.caption}
+                          <span className='clearfix' style={{position:'relative',bottom:'2px',float:'right'}}>
+                            {this.spanIfPresent(this.totalOrders(symbolObj.symbol), "label label-default", "total orders")}
+                            {this.spanIfPresent(this.long(symbolObj.symbol), "label label-success", "my long orders")}
+                            {this.spanIfPresent(this.short(symbolObj.symbol), "label label-warning", "my short orders")}
+                          </span>
+                        </a>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
             ))}
-          </ul>
+          </div>
         </div>
       </div>
     )
--- app/js/index.jsx
+++ app/js/index.jsx
@@ -7,133 +7,152 @@ var Symbols = [
     symbol: "AAPL",
     tv: "NASDAQ:AAPL",
     oracleArg: "AAPL",
-    oracleType: "Yahoo"
+    oracleType: "Yahoo",
+    group: "stocks"
   },
   {
     caption: "MSFT",
     symbol: "MSFT",
     tv: "NASDAQ:MSFT",
     oracleArg: "MSFT",
-    oracleType: "Yahoo"
+    oracleType: "Yahoo",
+    group: "stocks"
   },
   {
     caption: "AMZN",
     symbol: "AMZN",
     tv: "NASDAQ:AMZN",
     oracleArg: "AMZN",
-    oracleType: "Yahoo"
+    oracleType: "Yahoo",
+    group: "stocks"
   },
   {
     caption: "TSLA",
     symbol: "TSLA",
     tv: "NASDAQ:TSLA",
     oracleArg: "TSLA",
-    oracleType: "Yahoo"
+    oracleType: "Yahoo",
+    group: "stocks"
   },
   {
     caption: "NFLX",
     symbol: "NFLX",
     tv: "NASDAQ:NFLX",
     oracleArg: "NFLX",
-    oracleType: "Yahoo"
+    oracleType: "Yahoo",
+    group: "stocks"
   },
   {
     caption: "ETH/USD",
     symbol: "ETH/USD",
     tv: "KRAKEN:ETHUSD",
     oracleArg: "ETH",
-    oracleType: "CryptoUsd"
+    oracleType: "CryptoUsd",
+    group: "crypto/usd"
   },
   {
     caption: "BTC/USD",
     symbol: "BTC/USD",
     tv: "KRAKEN:XBTUSD",
     oracleArg: "BTC",
-    oracleType: "CryptoUsd"
+    oracleType: "CryptoUsd",
+    group: "crypto/usd"
   },
   {
     caption: "DASH/USD",
     symbol: "DASH/USD",
     tv: "KRAKEN:DASHUSD",
     oracleArg: "DASH",
-    oracleType: "CryptoUsd"
+    oracleType: "CryptoUsd",
+    group: "crypto/usd"
   },
   {
     caption: "ETC/USD",
     symbol: "ETC/USD",
     tv: "KRAKEN:ETCUSD",
     oracleArg: "ETC",
-    oracleType: "CryptoUsd"
+    oracleType: "CryptoUsd",
+    group: "crypto/usd"
   },
   {
     caption: "LTC/USD",
     symbol: "LTC/USD",
     tv: "KRAKEN:LTCUSD",
     oracleArg: "LTC",
-    oracleType: "CryptoUsd"
+    oracleType: "CryptoUsd",
+    group: "crypto/usd"
   },
   {
     caption: "XLM/USD",
     symbol: "XLM/USD",
     tv: "KRAKEN:XLMUSD",
     oracleArg: "XLM",
-    oracleType: "CryptoUsd"
+    oracleType: "CryptoUsd",
+    group: "crypto/usd"
   },
   {
     caption: "XMR/USD",
     symbol: "XMR/USD",
     tv: "KRAKEN:XMRUSD",
     oracleArg: "XMR",
-    oracleType: "CryptoUsd"
+    oracleType: "CryptoUsd",
+    group: "crypto/usd"
   },
   {
     caption: "ZEC/USD",
     symbol: "ZEC/USD",
     tv: "KRAKEN:ZECUSD",
     oracleArg: "ZEC",
-    oracleType: "CryptoUsd"
+    oracleType: "CryptoUsd",
+    group: "crypto/usd"
   },
   {
     caption: "REP/USD",
     symbol: "REP/USD",
     tv: "KRAKEN:REPUSD",
     oracleArg: "REP",
-    oracleType: "CryptoUsd"
+    oracleType: "CryptoUsd",
+    group: "crypto/usd"
   },
   {
     caption: "ETH/BTC",
     symbol: "ETH/BTC",
     tv: "KRAKEN:ETHXBT",
     oracleArg: "ETH",
-    oracleType: "CryptoBtc"
+    oracleType: "CryptoBtc",
+    group: "crypto/btc"
   },
   {
     caption: "DASH/BTC",
     symbol: "DASH/BTC",
     tv: "KRAKEN:DASHXBT",
     oracleArg: "DASH",
-    oracleType: "CryptoBtc"
+    oracleType: "CryptoBtc",
+    group: "crypto/btc"
   },
   {
     caption: "LTC/BTC",
     symbol: "LTC/BTC",
     tv: "KRAKEN:LTCXBT",
     oracleArg: "LTC",
-    oracleType: "CryptoBtc"
+    oracleType: "CryptoBtc",
+    group: "crypto/btc"
   },
   {
     caption: "MLN/BTC",
     symbol: "MLN/BTC",
     tv: "KRAKEN:MLNXBT",
     oracleArg: "MLN",
-    oracleType: "CryptoBtc"
+    oracleType: "CryptoBtc",
+    group: "crypto/btc"
   },
   {
     caption: "ZEC/BTC",
     symbol: "ZEC/BTC",
     tv: "KRAKEN:ZECXBT",
     oracleArg: "ZEC",
-    oracleType: "CryptoBtc"
+    oracleType: "CryptoBtc",
+    group: "crypto/btc"
   },
 ]
 
